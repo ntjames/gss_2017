3c950871fc7c5d14ffa56148a48151ad97fa673c
ntjames <nj1154@gmail.com>
2017-07-26 03:13:56 -0500

    add expenditure subcats

--- src/test_app_scrp.R
+++ src/test_app_scrp.R
@@ -5,13 +5,14 @@ wd<-switch(user,
            jacquelynneal=file.path("~jacquelynneal/Desktop/gss_2017"))
 
 setwd(wd)
-
 load(file.path(wd,"cache","com_plt.RData"))
 
+library(tidyverse)
+library(lubridate)
 library(shiny)
 library(plotly) # https://plot.ly/r/
 
-cats<-select(com_plt,cat1) %>% distinct() %>% flatten() %>% unlist()
+cats<-filter(com_plt,level<=2) %>% select(cat1) %>% distinct() %>% flatten() %>% unlist()
 
 ui <- fluidPage(
   
@@ -19,17 +20,20 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel( 
-      dateRangeInput("yr","Year", start = "2014-01-01", end = "2015-12-31",
+      dateRangeInput("yr","Year", start = "2013-01-01", end = "2015-12-31",
                      min ="2005-01-01", max="2015-12-31", 
                      startview="decade", format = "yyyy"),
       selectInput("cat","Category", 
-                  choices = c("Average annual expenditures","Food","Housing"),
+                  choices = cats,
                   multiple=TRUE, selected="Average annual expenditures"),
       uiOutput("ui"),
+      checkboxInput("showse","Show SE"),
       checkboxGroupInput("cuchar", "CU category",
                          c("All" = "all",
                            "Age" = "age",
-                           "Income" = "income")),
+                           "Income" = "inc",
+                           "Region" = "reg"),
+                         selected="all"),
       actionButton("plotButton", "Plot!")
     ),
     mainPanel(
@@ -45,32 +49,59 @@ server <- function(input, output) {
   output$ui<-renderUI({
     if (is.null(input$cat))
       return()
-
-  #  selsub<-unique(filter(com_plt, cat1 %in% input$cat) %>% select(subcat))
-   # selectInput("data", "Expenditure subcategory:", selsub, multiple=TRUE)
-    selectInput("data", "Expenditure subcategory:", 
-                list("foo"=c("a","b"),"bar"=c("c","d")), multiple=TRUE)
+    
+    #make and clean-up subcategory list
+    din<-data.frame(select(com_plt,lev2,lev3))
+    ulevs<-na.omit(unique(com_plt$lev2))
+    sublist<-lapply(ulevs, function(x) unique(subset(din,lev2==x,select=lev3,drop=T)))
+    names(sublist)<-ulevs
+    sublist<-lapply(sublist, function(x) x[!is.na(x)]) #rm NAs within lists
+    naonly<-sapply(sublist,function(x) all(is.na(x)) ) #rm items with only NA
+    sublist[naonly]<-NULL
+    
+    #keep items of sublist that match categories in input$cat
+    sublist2<-sublist[input$cat]
+    if ( all( sapply(sublist2,function(x) all(is.null(x))) ) )
+      return()
+    
+    naonly2<-sapply(sublist2,function(x) all(is.null(x)) ) #rm items with only NULL
+    sublist2[naonly2]<-NULL
+    
+    selectInput("subcat", "Expenditure subcategory:", sublist2 , multiple=TRUE)
   })
   
   
   # using ggplotly & eventReactive to only plot when all options set
   plt<-eventReactive(input$plotButton,{
-    p<-ggplot(filter(com_plt, cat1 %in% input$cat, cuchar %in% input$cuchar), 
-              aes(yr,Mean,group=cugrp))+geom_line(aes(color=factor(cugrp)))
     
-    # need to group by cugrp, cat1
-    # add checkbox for SE          
-    
-    # p<-ggplot(filter(com_plt, cat1 %in% input$cat), 
-    #           aes(yr,Mean,group=subcat))+
-    #   geom_line(aes(color=subcat))
-    
-    ggplotly(p)
+      yrs<-year(input$yr)
+      filtered_plt<-filter(com_plt, cat1 %in% c(input$cat,input$subcat), 
+             cuchar %in% input$cuchar, 
+             between(yr,yrs[1],yrs[2]))
+      
+      p<-ggplot(filtered_plt, aes(yr,Mean,group=interaction(cugrp,cat1),color=cugrp,linetype=cat1))+
+            geom_line() +  theme(legend.title=element_blank())
+      #https://github.com/chriddyp/ggplot2-plotly-cookbook/blob/master/legends.R
+      if (input$showse){
+        p<-p+geom_linerange(data=filtered_plt,aes(yr,ymin=Mean-SE,ymax=Mean+SE))
+      }
+      
+    # add additional years    
+    # append category name in from of cugrp  
+    # fix margins, axes, legends of plot
+
+
+    ggplotly(p,tooltip=c("x","y","colour","linetype"),width=1000,height=700)
   })
   
   output$plot <- renderPlotly({
     plt()
   })
+  
+  #use this to check input vals
+  # output$event <- renderPrint({
+  #  year(input$yr)
+  # })
   
 
 }
@@ -79,4 +110,7 @@ server <- function(input, output) {
 shinyApp(ui, server)
 
 
-#filter(com_plt, cat1 %in% "Average annual expenditures", cuchar %in% "all")
+#filter(com_plt, cat1 %in% "Average annual expenditures", cuchar %in% "inc")
+
+#ggplot(filter(com_plt, cat1 %in% c("Average annual expenditures"), cuchar %in% c("reg","all")), 
+#       aes(yr,Mean,group=interaction(cugrp,cat1),color=cat1,linetype=cugrp) )+ geom_line()
