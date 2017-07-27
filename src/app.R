1de5c4d12a6f51dacd6afea884de0a88d34b1280
ntjames <nj1154@gmail.com>
2017-07-27 02:38:32 -0500

    updates to tab1

--- src/app.R
+++ src/app.R
@@ -13,11 +13,7 @@ setwd(wd)
 library(shiny)
 library(tidyverse)
 library(magrittr)
-
-# use %<>% from magrittr
-#names(xpa15) %<>% toupper
-#dat<- data.frame(expn)
-#dat$qyear <- factor(dat$qyear,levels=c('20151','20152','20153','20154','20161'),labels=c('Q1','Q2','Q3','Q4','Q5'))
+library(DT)
 
 #load data
 load(file.path(wd,"cache","fmli.RData"))
@@ -29,7 +25,7 @@ ui <- fluidPage(
    # Application title
    #titlePanel("Consumer Expenditure Data"),
    
-navbarPage("Consumer Expenditure Data",
+navbarPage("Consumer Expenditure Data", selected="Descriptives", #temp make Descriptives selected
   
   ### Tab 0 input ###
   tabPanel("About",
@@ -46,24 +42,19 @@ navbarPage("Consumer Expenditure Data",
       sidebarPanel(
         selectInput("data_t1",
                      "Choose a dataset:",
-                     choices=c("FMLI"="fmli")),
-        selectInput("year_t1",
-                    "Choose a year:",
-                    choices=c("2015"="15","2016"="16")),        
-        selectInput("qtr_t1",
-                     "Choose a quarter:",
-                     choices=c("1x","1")),
-        selectInput("var_t1",
-                    "Choose a variable:",
-                    choices=c("childage","rwaterpc")),
+                     choices=c("FMLI"="fmli","MEMI"="memi")),
+        uiOutput("ui_year_t1"),
+        uiOutput("ui_qtr_t1"),
+        uiOutput("ui_var_t1"),       
       #  numericInput("obs_t1", "Number of observations to view:", 5),
         actionButton("dispButton_t1", "Display")
       ),
       
       mainPanel(
-        verbatimTextOutput("summary_t1"),
-        tableOutput("view_t1"),
-        plotOutput("distPlot_t1"),
+        tableOutput("var_t1"),
+        dataTableOutput("codes_t1"),
+        tableOutput("summ_t1"),
+        plotOutput("plot_t1"),
         verbatimTextOutput("event")
       )
       
@@ -77,12 +68,12 @@ navbarPage("Consumer Expenditure Data",
       sidebarPanel(
         selectInput("variable_t2",
                     "Choose a variable:",
-                    choices=names(dat)),
+                    choices=c("a","b")),
         numericInput("obs_t2", "Number of observations to view:", 5)
       ),
              
       mainPanel(
-        verbatimTextOutput("summary_t2"),
+        htmlOutput("summary_t2"),
         tableOutput("view_t2"),
         plotOutput("distPlot_t2")
       )
@@ -90,9 +81,23 @@ navbarPage("Consumer Expenditure Data",
   ), # close tab 2 input
   
   ### Tab 3 input ###
-  tabPanel("Annual"
-           
-  )
+  tabPanel("Annual",
+    sidebarLayout(
+     
+      sidebarPanel(
+        selectInput("variable_t3",
+                   "Choose a variable:",
+                   choices=c("a","b")),
+        numericInput("obs_t3", "Number of observations to view:", 5)
+     ),
+     
+     mainPanel(
+       htmlOutput("summary_t3"),
+       tableOutput("view_t3"),
+       plotOutput("distPlot_t3")
+     )
+   ) # close sidebarLayout
+) # close tab 3 input
   
 ) # close navbarPage
 ) # close ui fluidPage
@@ -100,70 +105,148 @@ navbarPage("Consumer Expenditure Data",
 #### Define server ####
 server <- function(input, output) {
   
-  #input$data_t1
-  #input$year_t1
-  #input$qtr_t1
-  #input$var_t1
+### tab 1 outputs ###
+
+#dynamic UI  
+  output$ui_year_t1<-renderUI({
+    if (is.null(input$data_t1))
+      return()
+    
+    yrvec0<-select(get(input$data_t1),fileyear) %>% distinct() %>% pull() 
+    yrvec<-paste0("20",yrvec0)
+    yrls<-as.list(yrvec0)
+    names(yrls)<-yrvec
+    
+    selectInput("year_t1", "Choose a year:", yrls)
+  })
+
+  output$ui_qtr_t1<-renderUI({
+    if (is.null(input$data_t1)|is.null(input$year_t1))
+      return()
+    
+    qtrvec<-filter(get(input$data_t1),fileyear==input$year_t1) %>%
+      select(fileqtr) %>% distinct() %>% pull() 
+    
+    selectInput("qtr_t1", "Choose a quarter:", qtrvec)
+  })
+
+  output$ui_var_t1<-renderUI({
+    if (is.null(input$data_t1))
+      return()
+    
+    # drop flag variables
+    flgvars<- select(data_dic_vars,contains("Flag")) %>% distinct() %>% pull() %>% tolower()
+      
+    varvec<-filter(get(input$data_t1),fileyear==input$year_t1,fileqtr==input$qtr_t1) %>% 
+      select(-one_of(flgvars)) %>% names()
+    
+    ## append names
+    vv2 <-as.tibble(data_dic_vars) %>%
+          filter(File==toupper(input$data_t1),
+                 `Variable Name` %in% toupper(varvec),
+                  as.integer(paste0("20",input$year_t1))>=`First  Year`,
+                  as.integer(paste0("20",input$year_t1))<=`Last  Year`) %>% 
+          select('Variable Name','Variable Description') %>%
+          mutate(vname=paste(`Variable Name`,"(",`Variable Description`,")"))
+    
+    varlist<-select(vv2,'Variable Name') %>% pull()  %>% tolower() %>% as.list()
+     
+    varlab<- select(vv2,vname) %>% pull()
+    names(varlist)<-varlab
+    
+    selectInput("var_t1", "Choose a variable:", varlist)
+  })  
   
-  t1<-eventReactive(input$dispButton_t1,{
-    data_t1_upper<- toupper(input$data_t1) 
-    var_t1_upper<-  toupper(input$var_t1) 
+    
+# display button reactive  
+t1<-eventReactive(input$dispButton_t1,{
+    
+  data_t1_upper <- toupper(input$data_t1) 
+  var_t1_upper <- toupper(input$var_t1) 
+  
   # filter & select data
-  
   # file, year (& qtr) determines available vars to select
-  #  fdat_t1<- filter(eval(parse(text=input$data_t1)),fileyear==input$year_t1,fileqtr==input$qtr) %>% 
-  #    select(input$var_t1) %>% as.tibble()
-    # fdat_t1<-filter(get(input$data_t1),fileyear==input$year_t1) %>% 
-    #   slice(1:5) %>%
-    #   select(input$var_t1)
-    # fdat_t1[[1]] %>% typeof()
-  fdat_t1<- filter(get(input$data_t1),fileyear==input$year_t1,fileqtr==input$qtr_t1) %>%
+  fdat_t1<-filter(get(input$data_t1),fileyear==input$year_t1,fileqtr==input$qtr_t1) %>%
    select(input$var_t1)
   
   #determine var type
- vartype_t1<-fdat_t1[[1]] %>% typeof()
- catvar_t1<-switch(vartype_t1,
+  vartype_t1<-fdat_t1[[1]] %>% typeof()
+  catvar_t1<-switch(vartype_t1,
                 character=TRUE,
-                integer=FALSE)
+                integer=FALSE,
+                double=FALSE,
+                logical=TRUE)
 
-  # filter info from data dictionary
- fdv_t1<-filter(data_dic_vars,File==data_t1_upper ,`Variable Name`==var_t1_upper)
+  # filter info from data dictionary !!for current year
+  fdv_t1<-filter(data_dic_vars,File==data_t1_upper ,`Variable Name`==var_t1_upper,
+                as.integer(paste0("20",input$year_t1))>=`First  Year`,
+                as.integer(paste0("20",input$year_t1))<=`Last  Year`)
  
+  out<-list(fdat_t1=fdat_t1,catvar_t1=catvar_t1,fdv_t1=fdv_t1)
   
-  #filter info from codes (if applicable?)
- # if (catvar_t1){
- #   fdc_t1<-filter(data_dic_codes,File==data_t1_upper,`Variable Name`==var_t1_upper)
- # 
- #   fdat_fc_t1<-factor(fdat_t1[[1]],
- #                   levels=unlist(flatten(select(fdc_t1,`Code Value`))),
- #                   labels=unlist(flatten(select(fdc_t1,`Code Description`))))
- #  }
+  #filter info from codes (if cat) 
+  #!! check if filtered exists e.g. newid is char, but has no codes 
+  #!! some vars in data_dic_codes aren't exactly same as variables
+  #  (see e.g. BUILDING where dict needs to be padded to length 2 to match) 
+  if (catvar_t1){
+    fdc_t1<-filter(data_dic_codes,File==data_t1_upper,
+                   `Variable Name`==var_t1_upper,
+                   as.integer(paste0("20",input$year_t1))>=`First Year`,
+                   as.integer(paste0("20",input$year_t1))<=`Last Year`) %>%
+            select(`Variable Name`,`Code Value`,`Code Description`)
+  
+    if (nrow(fdc_t1)>0) {
+      fdat_fc_t1<-factor(fdat_t1[[1]],
+                    levels=unlist(flatten(select(fdc_t1,`Code Value`))),
+                    labels=unlist(flatten(select(fdc_t1,`Code Description`))))
+      } else { # character var w/o code (e.g. newid)
+        fdat_fc_t1<-factor(fdat_t1[[1]])
+      }
+    out <- append(out, list(fdc_t1=fdc_t1,fdat_fc_t1=fdat_fc_t1))    
+  }
+
+out 
 })
   
-### tab 1 outputs ###
-output$summary_t1 <- renderPrint({
-  t1()
-  #display data dict info
- # data.frame(fdv_t1)
-#  if(catvar_t1){
-#    data.frame(fdc_t1)
-#  }
-})
+
+# display data dictionary variable info
+output$var_t1<-renderTable({
+  t1<-t1()
+  t1$fdv_t1
+})  
   
-#output$view_t1 <- renderTable({
-#  t1()
-  #display summary
-#   if (catvar_t1){
-#     table(fdat_fc_t1)
-#   } else {
-#     summary(fdat_t1[[1]])
-#   }
-#   
-# })
+# if categorical var, display codes from data dictionary
+output$codes_t1 <- renderDataTable({
+  t1<-t1()
+  if(t1$catvar_t1){ df<-data.frame(t1$fdc_t1) 
+  }  else { return() }
+},rownames= FALSE)
+  
+#display summary
+output$summ_t1 <- renderTable({
+  t1<-t1()
+  if (t1$catvar_t1){
+    #tbl<-table(t1$fdat_fc_t1) %>% rename(foo=Var1)
+    group_by(t1$fdat_t1[1],t1$fdat_fc_t1) %>% dplyr::summarize(Freq=n()) %>% 
+      rename(Category=`t1$fdat_fc_t1`)
+   # str(tbl)
+  } else { #!don't reverse order of t1 & t2 na.omit has weird residual behavior
+    t2<-t1$fdat_t1[1] %>% dplyr::summarize(n=n(),`NA`= sum(is.na( eval(parse(text=isolate(input$var_t1))) )))
+    t1<-summarize_all(na.exclude(t1$fdat_t1[1]),funs(min,mean,max,sd,IQR))   
+    c(t1,t2)
+  }
+})
    
-output$distPlot_t1 <- renderPlot({
-#  filter(dat,subcat==input$subcatvar_t1) %>% group_by(qyear) %>% 
-#    ggplot(aes(qyear,get(input$variable_t1),group=1)) + geom_line()
+#display plot
+output$plot_t1 <- renderPlot({
+  t1<-t1()
+  if (t1$catvar_t1){
+    ggplot(as.tibble(t1$fdat_fc_t1),aes(value))+geom_bar()+
+      theme(axis.text.x = element_text(angle = 30, hjust = 1))
+  } else {
+    ggplot(t1$fdat_t1,aes(eval(parse(text=isolate(input$var_t1))) )) + geom_density()
+  }
+  
 })
   
 #use this to check input vals
