c1f0095ea1d5b2169352579d38f235b5a3c804da
ntjames <nj1154@gmail.com>
2017-07-28 03:52:26 -0500

    process all major datasets

--- src/cln_data.R
+++ src/cln_data.R
@@ -19,9 +19,8 @@ library(readxl)
 # only load cache
 # load.project(list('dat_loading'=FALSE,'munging'=FALSE)) 
 
-#currently use only CESD PUMD Interview files
-#! add major Diary files
 if (0) {
+#Interview
   # FMLI - CU characteristics, income, and summary level expenditures (1 record per CU)
   # MEMI - member characteristics, income data 
   # MTBI - monthly expenditures at UCC level (multiple records per CU - 
@@ -36,59 +35,78 @@ if (0) {
   #         contain UCCs & abbreviated titles
    
   # EXPN - 43 detailed expenditure files
+
+# Diary
+  # FMLD - Contains CU chars, income, and chars and earnings of the ref person and spouse.    
+  # MEMD - Member Characteristics and Income" 
+  # DTBD - contains CU characteristic and income data.
+  # EXPD - expenditure recorded by a CU in a weekly diary is identified by UCC
+  # DTID -  similar to the DTBD file, w/ imputation
+  
+  #DSTUB
 }
+
+## function to append major datasets
+read_app<-function(pre){
+  pat<-paste0(pre,"[0-9]")
+  files<-ls(envir=.GlobalEnv)[grep(pat,ls(envir=.GlobalEnv))]
+  if (pre=="ntaxi"){ pos<-c(6,9)  } else { pos<-c(5,8) }
+  
+  #add file name as variable, then use separate to split into year and quarter
+  appfiles<-bind_rows(lapply(files, function(x) mutate(get(x),fileyrqtr=str_sub(x,pos[1],pos[2]) ))) %>%
+    separate(fileyrqtr,into=c("fileyear","fileqtr"),sep=2)
+  assign(pre,appfiles, envir=.GlobalEnv)
+}
+
+## Interview ## 
+majds_i<-list("fmli","memi","mtbi","itbi","itii","ntaxi")
+tmp<-lapply(majds_i,read_app)
+
+# delete all of the independent data frames from memory
+pat_i<-paste0(unlist(majds_i),"[0-9]",collapse="|")
+ind_files<-ls()[grep(pat_i,ls())]
+rm(list=ind_files)
+
+# clear up RAM
+gc()
+
+#process paradata
+pards_i<-c("fpar","mchi")
+
+mchi<-as.tibble(mchi1415) %>% separate(qyear,into=c("fileyear","fileqtr"),sep=4) %>% 
+  mutate(fileyear=str_sub(fileyear,3,4))
+fpar<-as.tibble(fpar1415) %>% separate(qyear,into=c("fileyear","fileqtr"),sep=4) %>% 
+  mutate(fileyear=str_sub(fileyear,3,4))
+
+int_files<-c(unlist(majds_i),pards_i)
+
+## Diary ##
+majds_d<-list("fmld","memd","dtbd","expd","dtid")
+tmp<-lapply(majds_d,read_app)
+dry_files<-unlist(majds_d)
+
+#save major datasets
+major_ds<-c(int_files,dry_files)
+save(list=major_ds,file=file.path(wd,"cache","maj.RData"))
 
 
 #read in data dictionary 
 stubpath<-file.path(getwd(),"stubdata")
 
 data_dic_vars<-read_excel(file.path(stubpath,"ce_pumd_interview_diary_dictionary.xlsx"), 
-                     sheet=2, col_names = TRUE, skip=2)
+                          sheet=2, col_names = TRUE, skip=2)
 
 data_dic_codes<-read_excel(file.path(stubpath,"ce_pumd_interview_diary_dictionary.xlsx"), 
-                          sheet=3, col_names = TRUE, skip=2)
+                           sheet=3, col_names = TRUE, skip=2)
 
 save(data_dic_vars,data_dic_codes,file=file.path(wd,"cache","dict.RData"))
 
-## read-in major datasets, append
-
-# fmli_files<-ls()[grep("fmli[0-9]",ls())]
-
-#add file name as variable for each FMLI, then use separate to split into year and quarter
-#fmli<-bind_rows(lapply(fmli_files, function(x) mutate(get(x),fileyrqtr=str_sub(x,5,8)  ))) %>%
-#  separate(fileyrqtr,into=c("fileyear","fileqtr"),sep=2)
-
-#save(fmli,file=file.path(wd,"cache","fmli.RData"))
-
-read_app<-function(pre){
-  pat<-paste0(pre,"[0-9]")
-  files<-ls(envir=.GlobalEnv)[grep(pat,ls(envir=.GlobalEnv))]
-    
-  #add file name as variable, then use separate to split into year and quarter
-  appfiles<-bind_rows(lapply(files, function(x) mutate(get(x),fileyrqtr=str_sub(x,5,8) ))) %>%
-    separate(fileyrqtr,into=c("fileyear","fileqtr"),sep=2)
-  assign(pre,appfiles, envir=.GlobalEnv)
-}
-
-read_app("fmli")
-
-majds<-list("fmli","memi","itbi")
-
-tmp<-lapply(majds,read_app)
-
-# delete all of the independent data frames from memory
-# rm(list=fmli_files)
-
-# clear up RAM
-gc()
-
-
-save(list=unlist(majds),file=file.path(wd,"cache","maj.RData"))
 
 
 
+if (0){
 ## STEP 1: READ IN THE STUB PARAMETER FILE AND CREATE FORMATS  
-#?  need to modify for multiple files?
+#?  need to modify for multiple files??
 stubfn<-dir(stubpath,"*.txt")
 
 #get wrapped rows, append X3 in wrapped row to X3 in previous row 
@@ -105,66 +123,24 @@ stubfile0[w1,"title"]<-paste(unlist(stubfile0[w1,"title"]),unlist(stubfile0[w2,"
 stubfile<-filter(stubfile0, type==1, source != "T",
                group %in% c("CUCHARS", "FOOD", "EXPEND", "INCOME")) %>%
           mutate(count=9999+1:nrow(.),line=paste0(count,level))
-
-# start with a character vector with ten blank strings..
-curlines <- rep( "" , 10 )
-
-# initiate a matrix containing the line numbers of each expenditure category
-aggfmt0 <- matrix( nrow = nrow( stubfile ) , ncol = 10 )
-
-# loop through each record in the stubfile..
-for ( i in seq( nrow( stubfile ) ) ){
-  
-  # if the 'UCC' variable is numeric 
-    if (  identical(as.character(stubfile[i,"source"]),"I") ){
-    
-    # save the line number as the last element in the char vector
-    curlines[ 10 ] <- unlist(stubfile[ i , "line" ])
-    
-    # otherwise blank it out
-  } else curlines[ 10 ] <- ""
-  
-  # store the current line and level in separate atomic variables
-  curlevel <- unlist(stubfile[ i , "level" ])
-  
-  # write the current line inside the length-ten character vector
-  curlines[ curlevel ] <- unlist(stubfile[ i , "line" ])
-  
-  # if the current level is 1-8, blank out everything above it up to nine
-  if ( curlevel < 9 ) curlines[ (curlevel+1):9 ] <- ""
-  
-  # remove actual value
-  savelines <- curlines
-  savelines[ curlevel ] <- ""
-  
-  # overwrite the entire row with the character vector of length ten
-  aggfmt0[ i , ] <- savelines
 }
-
-# convert the matrix to a data frame and name columns line1 - line10
-# tack on the UCC and line columns from the stubfile (which has the same number of records)
-# remove records where the UCC is not numeric, order by UCC, rename line to compare
-aggfmt1 <- as_tibble(aggfmt0) %>% setNames(paste0( "line" , 1:10 )) %>%
-      bind_cols(select(stubfile,var_ucc,line,source)) %>%
-      filter(source=="I") %>% arrange(var_ucc) %>% rename(compare = line) %>%
-      mutate(source=NULL)
-
-# reset the row names/numbers
-#rownames( aggfmt1 ) <- NULL
-
-# transpose the data, holding UCC and compare
-aggfmt2 <- melt(data.frame(aggfmt1), id=c("var_ucc","compare"))
-names( aggfmt2 )[ 4 ] <- "line"
-
-# retain the UCC-to-line crosswalk wherever the 'line' variable is not blank
-aggfmt <- subset( aggfmt2 , line != "" , select = c( "var_ucc" , "line" ) )
-
-# re-order the data frame by UCC
-aggfmt <- aggfmt[ order( aggfmt$var_ucc ) , ]
-
 
 
 #scratch below
+
+# fmli_files<-ls()[grep("fmli[0-9]",ls())]
+
+#add file name as variable for each FMLI, then use separate to split into year and quarter
+#fmli<-bind_rows(lapply(fmli_files, function(x) mutate(get(x),fileyrqtr=str_sub(x,5,8)  ))) %>%
+#  separate(fileyrqtr,into=c("fileyear","fileqtr"),sep=2)
+
+#save(fmli,file=file.path(wd,"cache","fmli.RData"))
+
+#read_app("fmli")
+
+#majds<-list("fmli","memi","itbi")
+
+
 
 # 
 # #filter(data_dic_codes,File=='FMLI',`Variable Name`=='RWATERPC')
@@ -230,3 +206,62 @@ aggfmt <- aggfmt[ order( aggfmt$var_ucc ) , ]
 # ggplot(fdat,aes(eval(parse(text=var)))) + geom_density()
 # 
 # select(data_dic_vars,contains("Flag")) %>% distinct() %>% p
+
+
+# # start with a character vector with ten blank strings..
+# curlines <- rep( "" , 10 )
+# 
+# # initiate a matrix containing the line numbers of each expenditure category
+# aggfmt0 <- matrix( nrow = nrow( stubfile ) , ncol = 10 )
+# 
+# # loop through each record in the stubfile..
+# for ( i in seq( nrow( stubfile ) ) ){
+#   
+#   # if the 'UCC' variable is numeric 
+#     if (  identical(as.character(stubfile[i,"source"]),"I") ){
+#     
+#     # save the line number as the last element in the char vector
+#     curlines[ 10 ] <- unlist(stubfile[ i , "line" ])
+#     
+#     # otherwise blank it out
+#   } else curlines[ 10 ] <- ""
+#   
+#   # store the current line and level in separate atomic variables
+#   curlevel <- unlist(stubfile[ i , "level" ])
+#   
+#   # write the current line inside the length-ten character vector
+#   curlines[ curlevel ] <- unlist(stubfile[ i , "line" ])
+#   
+#   # if the current level is 1-8, blank out everything above it up to nine
+#   if ( curlevel < 9 ) curlines[ (curlevel+1):9 ] <- ""
+#   
+#   # remove actual value
+#   savelines <- curlines
+#   savelines[ curlevel ] <- ""
+#   
+#   # overwrite the entire row with the character vector of length ten
+#   aggfmt0[ i , ] <- savelines
+# }
+# 
+# # convert the matrix to a data frame and name columns line1 - line10
+# # tack on the UCC and line columns from the stubfile (which has the same number of records)
+# # remove records where the UCC is not numeric, order by UCC, rename line to compare
+# aggfmt1 <- as_tibble(aggfmt0) %>% setNames(paste0( "line" , 1:10 )) %>%
+#       bind_cols(select(stubfile,var_ucc,line,source)) %>%
+#       filter(source=="I") %>% arrange(var_ucc) %>% rename(compare = line) %>%
+#       mutate(source=NULL)
+# 
+# # reset the row names/numbers
+# #rownames( aggfmt1 ) <- NULL
+# 
+# # transpose the data, holding UCC and compare
+# aggfmt2 <- melt(data.frame(aggfmt1), id=c("var_ucc","compare"))
+# names( aggfmt2 )[ 4 ] <- "line"
+# 
+# # retain the UCC-to-line crosswalk wherever the 'line' variable is not blank
+# aggfmt <- subset( aggfmt2 , line != "" , select = c( "var_ucc" , "line" ) )
+# 
+# # re-order the data frame by UCC
+# aggfmt <- aggfmt[ order( aggfmt$var_ucc ) , ]
+# 
+
